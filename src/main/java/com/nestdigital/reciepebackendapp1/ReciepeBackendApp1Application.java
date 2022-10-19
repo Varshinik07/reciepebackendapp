792e4940ebcf9e5a54c1131465ffaec3457219bc
Varshini <varshuk01@gmail.com>
2022-10-19 19:02:01 +0530

    message

---
+++ src/main/java/com/nestdigital/reciepebackendapp1/ReciepeBackendApp1Application.java
@@ -0,0 +1,13 @@
+package com.nestdigital.reciepebackendapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciepeBackendApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciepeBackendApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/reciepebackendapp1/ReciepeBackendApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.reciepebackendapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciepeBackendApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
